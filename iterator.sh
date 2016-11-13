273d59e458fefd01b6901b22c7364827d26ccf09
Akshit Tyagi <akty.tyagi7@gmail.com>
2016-11-13 14:18:20 +0530

    stashing iterator.sh

--- iterator.sh
+++ iterator.sh
@@ -2,7 +2,7 @@
 COUNTER=0
 while [  $COUNTER -lt 100 ]; do
 	echo The counter is $COUNTER
-	num=$[COUNTER+25000]
+	num=$[COUNTER+20000]
 	timeout 120s ./learner.sh $num
 	#PID=`ps -eaf | grep run1.sh | grep -v grep | awk '{print $2}'`
 	#if [[ "" !=  "$PID" ]]; then
